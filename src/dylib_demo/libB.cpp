012a65338d905dc221dfb96993ae7f4256f656d8
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2026-03-06 00:17:39 -0500

    make `libB` and `libC` "runnable" by the `dylib` command

--- src/dylib_demo/libB.cpp
+++ src/dylib_demo/libB.cpp
@@ -5,3 +5,10 @@ extern "C" __attribute__((visibility("default"))) int ndsh_libB_value(int value)
 	std::printf("libB: ndsh_libB_value(%d)\n", value);
 	return value + 10;
 }
+
+extern "C" __attribute__((visibility("default"))) int ndsh_dylib_demo(int value)
+{
+	std::puts("libB ndsh_dylib_demo called!");
+	return 67;
+}
+
--- src/dylib_demo/libC.cpp
+++ src/dylib_demo/libC.cpp
@@ -5,3 +5,9 @@ extern "C" __attribute__((visibility("default"))) int ndsh_libC_value(int value)
 	std::printf("libC: ndsh_libC_value(%d)\n", value);
 	return value + 20;
 }
+
+extern "C" __attribute__((visibility("default"))) int ndsh_dylib_demo(int value)
+{
+	std::puts("libC ndsh_dylib_demo called!");
+	return 69;
+}
